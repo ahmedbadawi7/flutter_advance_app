ff662e676d8e2585863c4dd16483244364c72c1a
ahmedbadawi7 <ahmedbadawi7@outlook.com>
2024-04-29 22:18:41 +0300

    start edit

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'flutter advanced corss',
             ),
             Text(
               '$_counter',
