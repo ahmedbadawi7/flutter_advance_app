64a39bab66ad5eefa6de9d6919fcd958828191a8
ahmedbadawi7 <ahmedbadawi7@outlook.com>
2024-05-02 23:27:47 +0300

    test

--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -25,7 +25,7 @@ class OnboardingScreen extends StatelessWidget {
                   child: Column(
                     children: [
                       Text(
-                        'Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.',
+                        'Manage annd schedule all of your medical appointments easily with Docdoc to get a new experience.',
                         style: TextStyles.font13GrayRegular,
                         textAlign: TextAlign.center,
                       ),
