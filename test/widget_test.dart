91223c231b350c40727ec288e0227b63be1ab185
ahmedbadawi7 <ahmedbadawi7@outlook.com>
2024-05-03 00:25:09 +0300

    no message

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,8 +10,6 @@ import 'package:advansed_app/features/doc_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:advansed_app/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
